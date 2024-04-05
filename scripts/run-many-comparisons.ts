8790d5907c1fbaf2b7e0e29c1531a2356bebceb5
Darren Kelly <darren.kelly@vechain.org>
2024-04-05 12:44:55 +0100

    feat: add run many script

---
+++ scripts/run-many-comparisons.ts
@@ -0,0 +1,41 @@
+import util from "util";
+import {exec as execSync} from "child_process";
+import {config} from "dotenv";
+
+const exec = util.promisify(execSync);
+config();
+
+const tests = [
+  "log-events-by-address.test.js",
+  "log-events-by-no-criteria-with-range.test.js",
+  "log-events-by-no-criteria.test.js",
+  "log-events-by-topic0.test.js",
+  "log-events-by-topic1.test.js",
+  "log-events-by-topic2.test.js",
+  "log-events-by-topic3.test.js",
+  "log-events-by-vtho-recipient.test.js",
+  "log-events-by-vtho-sender.test.js",
+  "log-transfers-by-recipient.test.js",
+  "log-transfers-by-sender.test.js",
+  "log-transfers-by-txOrigin.test.js",
+  "log-transfers-no-criteria.test.js"
+]
+
+const start = async () => {
+    for (const test of tests) {
+        try {
+
+            console.log(`=================================================================================`)
+            console.log(`Running ${test}....`)
+
+            const { stdout } = await exec(`yarn test ${test}`);
+
+            console.log(stdout)
+            console.log('\n')
+        } catch (error) {
+            console.error(`Error running ${test}`, error);
+        }
+    }
+}
+
+start()
