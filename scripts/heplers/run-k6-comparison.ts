ac4429e4361b19aa526f223fdcfb951960060d89
Darren Kelly <darren.kelly@vechain.org>
2024-04-08 15:00:17 +0100

    remove test sleep

--- scripts/heplers/run-k6-comparison.ts
+++ scripts/heplers/run-k6-comparison.ts
@@ -81,11 +81,6 @@ export const runK6Comparison = async (testName: string) => {
   console.log(`\tIterations: ${params.amount}`);
 
   for (let i = 0; i < params.amount; i++) {
-    if (i > 0) {
-      console.log("\n\n\tCooling down for 4 minutes...");
-      await new Promise((resolve) => setTimeout(resolve, 240_000));
-    }
-
     const command1 = `k6 run --env NODE_URL=${params.node1} --env NETWORK=${params.network} ${params.script}`;
     console.log(`\n\tRunning k6 (iteration=${i + 1}) on node 1...`);
     await executeCommand(command1);
